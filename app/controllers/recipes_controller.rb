d0e0d6fbfe3c3ca55c843e4d2d1ce27413e876fa
theory friction practice <jackson.brebnor@gmail.com>
2016-02-16 16:55:56 -0500

    reorients controller to run recipes through user

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,12 +1,12 @@
 class RecipesController < ApplicationController
   before_action :set_recipe, only: [:show, :edit, :update, :destroy]
-  load_and_authorize_resource :through => :user, except: [:index]
+  load_and_authorize_resource :through => :current_user, except: [:index]
 
 
   # GET /recipes
   # GET /recipes.json
   def index
-    @recipes = Recipe.all
+    @recipes = recipes.all
   end
 
   # GET /recipes/1
@@ -16,7 +16,7 @@ class RecipesController < ApplicationController
 
   # GET /recipes/new
   def new
-    @recipe = Recipe.new
+    @recipe = recipes.new
   end
 
   # GET /recipes/1/edit
@@ -26,7 +26,7 @@ class RecipesController < ApplicationController
   # POST /recipes
   # POST /recipes.json
   def create
-    @recipe = Recipe.new(recipe_params)
+    @recipe = recipes.new(recipe_params)
 
     respond_to do |format|
       if @recipe.save
@@ -64,13 +64,16 @@ class RecipesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_recipe
-      @recipe = Recipe.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_recipe
+    @recipe = Recipe.find(params[:id])
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def recipe_params
-      params.require(:recipe).permit(:title, :description, :user_id)
-    end
+  def recipes
+    current_user ? current_user.recipes : Recipe
+  end
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def recipe_params
+    params.require(:recipe).permit(:title, :description, :user_id)
+  end
 end
