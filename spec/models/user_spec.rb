d23bdb796362acd9131c789b37d86764d88b0bec
theory friction practice <jackson.brebnor@gmail.com>
2016-02-18 12:01:43 -0500

    basic rspec for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { build_stubbed(:user) }
+
+  describe 'attributes' do
+    it 'has an email address' do
+      expect(user.email).to be_a_kind_of(String)
+
+    end
+    it 'has an username' do
+      expect(user.username).to be_a_kind_of(String)
+
+    end
+    it 'has an password' do
+      expect(user.password).to be_a_kind_of(String)
+
+    end
+    describe 'admin' do
+      it 'has an admin' do
+        expect(user.attributes).to include("admin")
+      end
+      it 'defaults admin to false' do
+        expect(user.admin).to be false
+      end
+    end
+
+  end
 end
