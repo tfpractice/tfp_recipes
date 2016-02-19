d669d492497595963b9f37fb3f064ae70336a753
theory friction practice <jackson.brebnor@gmail.com>
2016-02-19 11:37:05 -0500

    add title validation to recipe and tests basic recpie controller

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,6 +3,7 @@ class Recipe < ActiveRecord::Base
   mount_uploader :image, ImageUploader
   has_many :ingredients
   has_many :directions, -> { order(position: :asc) }
+  validates_presence_of :title
   accepts_nested_attributes_for :ingredients, allow_destroy: true, reject_if: proc { |attributes| attributes['name'].blank? }
   accepts_nested_attributes_for :directions, allow_destroy: true, reject_if: proc { |attributes| attributes['step'].blank? }
 
